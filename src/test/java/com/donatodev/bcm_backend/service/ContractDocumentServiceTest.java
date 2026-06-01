d76d070832b27acdad56adfbaf9eeb8fec8438eb
DESKTOP-Q5CMCK9\Donato <donatocorbacio92@gmail.com>
2026-06-01 13:51:24 +0200

    fix: remove unused anyLong import in ContractDocumentServiceTest

--- src/test/java/com/donatodev/bcm_backend/service/ContractDocumentServiceTest.java
+++ src/test/java/com/donatodev/bcm_backend/service/ContractDocumentServiceTest.java
@@ -18,7 +18,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
